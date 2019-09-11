47d30d9c11588f3000ffcd8ee0bf7665e32b381e
ᏒᏊᏀᏋᏒ ᏕξᏐ <roger.sei@i9xp.com.br>
2019-09-11 15:48:21 -0300

    Basic documentation for properties

--- src/Runner.php
+++ src/Runner.php
@@ -2,16 +2,61 @@
 
 namespace SupperGiggle;
 
+/**
+ * Núcleo do pacote GT8 framework.
+ *
+ * PHP Version 7
+ *
+ * @category  PHP
+ * @package   GT8
+ * @author    GT8 <contato@gt8.com.br>
+ * @copyright 2009-2019 GT8
+ * @license   //github.com/gt8/php-g-core/license GPL2
+ * @version   Release: GIT: 1
+ * @link      //github.com/gt8/php-g-core/
+ */
 class Runner
 {
+    /**
+     * Errors matched between git show and phpcs.
+     *
+     * @var array
+     */
     private $filesMatched = [];
+
+    /**
+     * Friendly separator displayed in terminal.
+     *
+     * @var string
+     */
     private $separator = PHP_EOL;
+
+    /**
+     * Arguments from CLI.
+     *
+     * @var array
+     */
     private $options = [];
 
+
+    /**
+     * Sets optional settings.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->separator = str_repeat('-', 110) . PHP_EOL;
     }
+
+
+    /**
+     * Run SupperGiggle, using arguments from CLI.
+     *
+     * @param array @args Arguments in CLI format
+     *
+     * @return void
+     */
     public function autoRun(array $args): void
     {
         next($args);
@@ -20,7 +65,7 @@ class Runner
         for ($arg=current($args); $arg; $arg=next($args)) {
             if (substr($arg, 0, 2) === '--' && strlen($arg) > 2) {
                 if (strpos($arg, '=') !== false) {
-                    preg_match('#--([\\w\\s]+)=("|\')?(.+)(\\2)?#', $arg, $results);
+                    preg_match('#--([\\w\\s\-]+)=("|\')?(.+)(\\2)?#', $arg, $results);
                     if (isset($results[2]) === true) {
                         $options[$results[1]] = $results[3];
                     } else {
@@ -46,14 +91,31 @@ class Runner
         $this->options = $options;
         $this->run();
     }
+
+
+    /**
+     * Helper to match a given string using regex.
+     *
+     * @param string $text  The input string
+     * @param string $regex The pattern to search for
+     *
+     * @return string The first string captured.
+     */
     protected function match(string $text, string $regex): string
     {
         preg_match("#$regex#", $text, $result);
         return $result[1] ?? $result[0] ?? '';
     }
+
+
+    /**
+     * Performs a git show and parse the results.
+     *
+     * @return array The parsed results in a bidimensional array.
+     */
     private function parseModifiedGitFiles(): array
     {
-        $result  = shell_exec("git --git-dir={$this->options['repo']}/.git --work-tree={$this->options['repo']} show {$this->options['commit']} --unified=0 | egrep '^(@@|\+\+\+)'");
+        $result  = shell_exec("git --git-dir={$this->options['repo']}/.git --work-tree={$this->options['repo']} {$this->options['check-type']} {$this->options['commit']} --unified=0 | egrep '^(@@|\+\+\+)'");
         $lines   = explode(PHP_EOL, $result);
         $crrFile = null;
         $files   = [];
@@ -72,9 +134,10 @@ class Runner
                 }
             }
         }
-
         return $files;
     }
+
+
     private function parsePHPCSErrors(string $path): array
     {
         $dir      = dirname(__FILE__);
@@ -108,18 +171,31 @@ class Runner
         $this->validateOptions();
 
         $files    = $this->parseModifiedGitFiles();
-        $checkAll = isset($this->options['full-check']);
-        foreach ($files as $file=>$gitErrors) {
+        $checkAll = isset($this->options['all']);
+        $checkSBC = 'Function closing brace must go on the next line';
+        foreach ($files as $file => $gitErrors) {
             foreach ($this->parsePHPCSErrors("{$this->options['repo']}/$file") as $crrPhpcsError) {
                 foreach ($gitErrors as $crrGitError) {
-                    if ($checkAll
-                        || ($crrPhpcsError['line'] >= $crrGitError['line'] && $crrPhpcsError['line'] <= ($crrGitError['line'] + $crrGitError['range']))
+                    if ($checkAll === true) {
+                        $this->printError($file, $crrPhpcsError);
+                        break;
+                    } elseif ($crrPhpcsError['line'] >= $crrGitError['line']
+                        && $crrPhpcsError['line'] <= ($crrGitError['line'] + $crrGitError['range'])
                     ) {
                         $this->printError($file, $crrPhpcsError);
+                    } elseif (($crrPhpcsError['line'] + 1) >= $crrGitError['line']
+                        && $crrPhpcsError['line'] <= ($crrGitError['line'] + $crrGitError['range'])
+                    ) {
+                        // Check for errors right after the line changed.
+                        // @gregsherwood suggestion for a better approach?
+                        if (strpos($crrPhpcsError['message'], $checkSBC) !== false) {
+                            $this->printError($file, $crrPhpcsError);
+                        }
                     }
                 }
             }
         }
+        
     }
     private function throw(string $message): void
     {
@@ -130,8 +206,17 @@ class Runner
         echo PHP_EOL;
         exit(-1);
     }
+
+
+    /**
+     * Validate all required fieldsand exit if it fails.
+     *
+     * @return void
+     */
     private function validateOptions(): void
     {
+        $this->options['check-type'] = ($this->options['check-type'] ?? 'show');
+
         if (empty($this->options['repo']) === true) {
             if (isset($this->options['repo']) === true) {
                 $this->throw('Empty value for "--repo"');
@@ -143,12 +228,21 @@ class Runner
         }
 
         if (empty($this->options['commit']) === true) {
-            if (isset($this->options['repo']) === true) {
-                $result = shell_exec("git --git-dir={$this->options['repo']}/.git --work-tree={$this->options['repo']} log --oneline --color | head -n 10");
-                $this->throw("Missing --commit.\nPlease, choose a commit or use --diff option to validate against the last change:\n$result");
+            if ($this->options['check-type'] === 'show') {
+                if (isset($this->options['repo']) === true) {
+                    $repo   = $this->options['repo'];
+                    $arg    = "git --git-dir=$repo/.git --work-tree=$repo log --oneline --color | head -n 10";
+                    $result = shell_exec($arg);
+                    $error  = "Missing --commit.\nPlease, choose a commit ";
+                    $error .= "or use --diff option to validate against the last change:\n$result";
+                    $this->throw($error);
+                }
+            } elseif ($this->options['check-type'] === 'diff') {
+                $this->options['commit'] = ($this->options['commit'] ?? '');
+            } else {
+                $this->thrown("Invalid value for --check-type.");
             }
-            
-            $this->throw('Missing --commit.');
         }
+
     }
 }
